ea3d5b541be628c056d34685059f92776c85bfa0
Pablo Cajigal <pmcajigaljr08@gmail.com>
2025-10-06 14:44:26 -0600

    Add project files.

---
+++ CoreLogic/Models/TankModel.cs
@@ -0,0 +1,23 @@
+﻿namespace CoreLogic.Models
+{
+    public class TankModel
+    {
+        public double Width { get; set; }
+        public double Depth { get; set; }
+        public double Height { get; set; }
+        public double WallThickness { get; set; }
+
+        public TankModel(double width, double depth, double height, double wallThickness)
+        {
+            Width = width;
+            Depth = depth;
+            Height = height;
+            WallThickness = wallThickness;
+        }
+
+        public override string ToString()
+        {
+            return $"Tank {Width}x{Depth}x{Height} mm - Thickness {WallThickness} mm";
+        }
+    }
+}
---
+++ CoreLogic/Services/TankBuilderService.cs
@@ -0,0 +1,32 @@
+﻿using CoreLogic.Models;
+using System.Collections.Generic;
+
+namespace CoreLogic.Services
+{
+    public class TankBuilderService
+    {
+        public TankModel Tank { get; private set; }
+        public List<SegmentModel> Segments { get; private set; }
+
+        public TankBuilderService(double width, double depth, double height, double wallThickness)
+        {
+            Tank = new TankModel(width, depth, height, wallThickness);
+            Segments = new List<SegmentModel>
+            {
+                new SegmentModel(SegmentType.Front) { HasATC = true },
+                new SegmentModel(SegmentType.Left) { HasMeterBox = true },
+                new SegmentModel(SegmentType.Rear) { HasRadiator = true },
+                new SegmentModel(SegmentType.Right) { HasDrainValve = true }
+            };
+        }
+
+        public string Summary()
+        {
+            string info = $"Tank: {Tank}\n";
+            foreach (var seg in Segments)
+                info += $" - {seg}\n";
+
+            return info;
+        }
+    }
+}
